5ea4e34add7160276f4c480421e513a1ef44a8af
noahschwad <noah.schwadron@gmail.com>
2016-10-31 15:53:24 -0600

    these files create and populate our dummy database

--- Equalsto.sql
+++ Equalsto.sql
@@ -1,41 +1,41 @@
-# This is the script to create the tables inside the database
+-- This is the script to create the tables inside the database
+-- DO NOT RUN THIS IF THE DATABASE IS POPULATED, IT WILL DELETE THE DATA
+-- run this and populate.sql with command: sqlite3 db.sqlite3 < <filename>
 
-DROP DATABASE IF EXISTS equalsto;
-CREATE DATABASE IF NOT EXISTS equalsto;
-USE equalsto;
+--DROP DATABASE IF EXISTS equalsto;
+--CREATE DATABASE IF NOT EXISTS equalsto;
+--USE equalsto;
 
-DROP TABLE IF EXISTS users ,
-                     questions,
-                     answers,
+DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     UserID      INT       NOT NULL,
     Username    VARCHAR(14)       NOT NULL,
     Firstname   VARCHAR(14)        NOT NULL,
-    lastname    VARCHAR(16)        NOT NULL,
-    gender      ENUM ('M','F')    NOT NULL,    
+    lastname    VARCHAR(16)        NOT NULL,    
     email        VARCHAR(40)      NOT NULL,
     PRIMARY KEY (UserID)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+);--ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
 
-
+DROP TABLE IF EXISTS questions;
 CREATE TABLE questions (
-    UserID            INT           NOT NULL,
+    --UserID            INT           NOT NULL,
     question_ID       int            NOT NULL,
     question_String   VARCHAR(40)     NOT NULL,
      answer1          VARCHAR(40)     NOT NULL,
      answer2          VARCHAR(40)     NOT NULL,
-     times_answered   INT             NOT NULL,
-     answered_yes     ENUM('Y','N')    NOT NULL,   
-     PRIMARY KEY (userID), REFERENCES users(userId)     --seems like primary key should be question_ID
+     --times_answered   INT             NOT NULL,
+     --answered_yes     ENUM('Y','N')    NOT NULL,   
+     PRIMARY KEY (question_ID)
+   --  FOREIGN KEY (UserID) REFERENCES users(UserID)
     
-)ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+);--ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
 
-
+DROP TABLE IF EXISTS answers;
 CREATE TABLE answers (
  
-     UserID           INT            NOT NULL,
-   answerSetID       INT             NOT NULL,
-    question_ID     INT             NOT NULL,
+   UserID       INT            NOT NULL,
+   --answerSetID  INT             NOT NULL,
+   --question_ID  INT             NOT NULL,
    answer1      VARCHAR(40)         NOT NULL,
    answer2      VARCHAR(40)         NOT NULL,
    answer3      VARCHAR(40)         NOT NULL,
@@ -46,9 +46,10 @@ CREATE TABLE answers (
    answer8      VARCHAR(40)         NOT NULL,
    answer9      VARCHAR(40)         NOT NULL,
    answer10     VARCHAR(40)         NOT NULL,
-    PRIMARY KEY (UserID), REFERENCES users(userId)
+   PRIMARY KEY (UserID),
+   FOREIGN KEY (UserID) REFERENCES users(UserID)
     
-)ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+);--ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
  
 
 
---
+++ populate.sql
@@ -0,0 +1,17 @@
+INSERT INTO users VALUES (1,"buddyboy","bud","boy","budboy@gmail.com");
+INSERT INTO users VALUES (2,"coolman","cool","man","coolman@gmail.com");
+INSERT INTO users VALUES (3,"bighuman","big","human","bighuman@gmail.com");
+INSERT INTO users VALUES (4,"birdperson","bird","person","bp@gmail.com");
+INSERT INTO users VALUES (5,"hereiam","i","am","crudboy@gmail.com");
+
+INSERT INTO questions VALUES (1,"waterfall or agile?","waterfall","agile");
+INSERT INTO questions VALUES (2,"9-5 or any schedule?","9-5","any schedule");
+INSERT INTO questions VALUES (3,"leader or follower?","leader","follower");
+INSERT INTO questions VALUES (4,"low risk or high reward?","low risk","high reward");
+INSERT INTO questions VALUES (5,"science or business?","science","business");
+
+INSERT INTO answers VALUES (1,"1","1","0","1","0","0","1","1","1","0");
+INSERT INTO answers VALUES (2,"1","0","0","1","0","0","1","1","1","0");
+INSERT INTO answers VALUES (3,"0","1","0","1","0","0","1","1","1","0");
+INSERT INTO answers VALUES (4,"1","1","0","1","0","0","1","1","0","0");
+INSERT INTO answers VALUES (5,"1","1","0","1","0","0","0","1","1","0");
